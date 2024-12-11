9ca102e07858d43e1a220b2dcee6ae512f1ab39f
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 12:06:11 +0100

    [update]:Lesson_2/Task_2-7/index.html, Main.ts, Main.js

---
+++ Lesson_2/Task_2-7/Main.ts
@@ -0,0 +1,21 @@
+interface ITimes {
+    time: number;
+}
+
+let clock: ITimes = {
+    time: 12
+};
+
+
+
+if(clock.time <= 15) {
+    console.log('The first part of an hour');
+} else if(clock.time <= 30) {
+    console.log('The second part of an hour');
+} else if(clock.time <= 45) {
+    console.log('The third part of an hour');
+} else if(clock.time <= 59) {
+    console.log('The fourth part of an hour');
+} else {
+    console.log('One hour has only 60 minutes');
+}
