940680f0df775e222c5a63d2b2005da19993de88
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 11:08:41 +0100

    [update]:Lesson_2/Task_2-1/index.html, Main.ts, Main.js

---
+++ Lesson_2/Task_2-1/Main.js
@@ -0,0 +1,2 @@
+var array = [123, 'name', -4, false, 'password', 8739, 'email', true, 3467, 'java script'];
+console.log(array);
---
+++ Lesson_2/Task_2-1/Main.ts
@@ -0,0 +1,5 @@
+interface IArray extends Array<string | number | boolean>{}
+
+
+let array: IArray = [123, 'name', -4, false, 'password', 8739, 'email', true, 3467, 'java script'];
+console.log(array);
