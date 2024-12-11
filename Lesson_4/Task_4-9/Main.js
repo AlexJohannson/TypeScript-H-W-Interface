47606b156e6efcab590bb76bf29199db3cc4d1fa
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 14:15:12 +0100

    [update]:Lesson_4/Task_4-9/index.html, Main.ts, Main.js

---
+++ Lesson_4/Task_4-9/Main.js
@@ -0,0 +1,29 @@
+var users = [
+    {
+        id: 1,
+        name: 'John',
+        age: 23
+    },
+    {
+        id: 2,
+        name: 'Oleksandr',
+        age: 30
+    },
+    {
+        id: 3,
+        name: 'Inna',
+        age: 32
+    },
+    {
+        id: 4,
+        name: 'Viktoria',
+        age: 25
+    }
+];
+arr(users);
+function arr(users) {
+    for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+        var user = users_1[_i];
+        document.write("<div>".concat(user.id, ". ").concat(user.name, " - ").concat(user.age, "</div>"));
+    }
+}
---
+++ Lesson_4/Task_4-9/Main.ts
@@ -0,0 +1,37 @@
+interface IUser {
+    id: number;
+    name: string;
+    age: number;
+}
+
+
+
+let users: IUser[] = [
+    {
+        id: 1,
+        name: 'John',
+        age: 23
+    },
+    {
+        id: 2,
+        name: 'Oleksandr',
+        age: 30
+    },
+    {
+        id: 3,
+        name: 'Inna',
+        age: 32
+    },
+    {
+        id: 4,
+        name: 'Viktoria',
+        age: 25
+    }
+]
+arr(users);
+
+function arr(users) {
+    for (const user of users) {
+        document.write(`<div>${user.id}. ${user.name} - ${user.age}</div>`)
+    }
+}
