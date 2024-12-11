1fc46d4ca4cb47cd0ecf0fe4217b20b406b443aa
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 17:15:27 +0100

    [update]:Lesson_6/Task_6-8/index.html, Main.ts, Main.js

---
+++ Lesson_6/Task_6-8/Main.ts
@@ -0,0 +1,61 @@
+interface IArrCourse {
+    title: string;
+    monthDuration: number;
+    hourDuration: number;
+    modules: string[];
+}
+
+
+
+
+let coursesArray: IArrCourse[] = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+
+const sass = coursesArray.filter(course => {
+    return course.modules.includes('sass');
+});
+
+
+const docker = coursesArray.filter(course => {
+    return course.modules.includes('docker');
+});
+
+console.log(sass);
+console.log(docker);
