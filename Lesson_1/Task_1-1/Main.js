413bed402a928e9a920bce5355ddea990e75c7bb
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 10:39:15 +0100

    [update]:Lesson_1/Task_1-1/index.html, Main.ts, Main.js

---
+++ Lesson_1/Task_1-1/Main.js
@@ -0,0 +1,28 @@
+var variables = {
+    a: 'hello',
+    b: 'owu',
+    c: 'com',
+    d: 'ua',
+    e: 1,
+    f: 10,
+    g: -999,
+    h: 123,
+    i: 3.14,
+    j: 2.7,
+    k: 16,
+    l: true,
+    m: false
+};
+console.log(variables.a);
+console.log(variables.b);
+console.log(variables.c);
+console.log(variables.d);
+console.log(variables.e);
+console.log(variables.f);
+console.log(variables.g);
+console.log(variables.h);
+console.log(variables.i);
+console.log(variables.j);
+console.log(variables.k);
+console.log(variables.l);
+console.log(variables.m);
---
+++ Lesson_1/Task_1-1/Main.ts
@@ -0,0 +1,46 @@
+interface IVariables {
+    a: string;
+    b: string;
+    c: string;
+    d: string;
+    e: number;
+    f: number;
+    g: number;
+    h: number;
+    i: number;
+    j: number;
+    k: number;
+    l: boolean;
+    m: boolean;
+
+}
+
+let variables: IVariables = {
+    a: 'hello',
+    b: 'owu',
+    c: 'com',
+    d: 'ua',
+    e: 1,
+    f: 10,
+    g: -999,
+    h: 123,
+    i: 3.14,
+    j: 2.7,
+    k: 16,
+    l: true,
+    m: false
+};
+
+console.log(variables.a);
+console.log(variables.b);
+console.log(variables.c);
+console.log(variables.d);
+console.log(variables.e);
+console.log(variables.f);
+console.log(variables.g);
+console.log(variables.h);
+console.log(variables.i);
+console.log(variables.j);
+console.log(variables.k);
+console.log(variables.l);
+console.log(variables.m);
