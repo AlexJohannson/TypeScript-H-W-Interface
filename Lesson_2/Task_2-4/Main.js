e2a52b6357d619b30a59c768efced28f4d51a2f2
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 11:29:49 +0100

    [update]:Lesson_2/Task_2-4/index.html, Main.ts, Main.js

---
+++ Lesson_2/Task_2-4/Main.js
@@ -0,0 +1,72 @@
+var users = [
+    {
+        id: 1,
+        name: 'Oleksandr',
+        username: 'Oleksandr_Oleksandr',
+        password: '@oleksandr'
+    },
+    {
+        id: 2,
+        name: 'Vasia',
+        username: 'Vasia_Vasia',
+        password: '@vasia'
+    },
+    {
+        id: 3,
+        name: 'Virginia',
+        username: 'Virginia_Virginia',
+        password: '@virginia'
+    },
+    {
+        id: 4,
+        name: 'Mykola',
+        username: 'Mykola_mykola',
+        password: '@mykola'
+    },
+    {
+        id: 5,
+        name: 'Vadym',
+        username: 'Vadym_Vadym',
+        password: '@vadym'
+    },
+    {
+        id: 6,
+        name: 'Dmytro',
+        username: 'Dmytro_Dmytro',
+        password: '@dmytro'
+    },
+    {
+        id: 7,
+        name: 'Inna',
+        username: 'Inna_Inna',
+        password: '@inna'
+    },
+    {
+        id: 8,
+        name: 'Svitlana',
+        username: 'Svitlana_Svitlana',
+        password: '@svitlana'
+    },
+    {
+        id: 9,
+        name: 'Olha',
+        username: 'Olha_Olha',
+        password: '@olha'
+    },
+    {
+        id: 10,
+        name: 'Roman',
+        username: 'Roman_Roman',
+        password: '@roman'
+    }
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
