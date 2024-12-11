73d498e1b2f7e1035937e462ffd996551bc1633f
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 12:12:45 +0100

    [update]:Lesson_2/Task_2-8/index.html, Main.ts, Main.js

---
+++ Lesson_2/Task_2-8/Main.js
@@ -0,0 +1,15 @@
+var numderDay = {
+    day: 14
+};
+if (numderDay.day <= 11) {
+    console.log('Your number ' + numderDay.day + ' is in the first decade of the month');
+}
+else if (numderDay.day <= 22) {
+    console.log('Your number ' + numderDay.day + ' is in the second decade of the month');
+}
+else if (numderDay.day <= 31) {
+    console.log('Your number ' + numderDay.day + ' is in the third decade of the month');
+}
+else {
+    console.log('You entered an incorrect number ' + numderDay.day + ' a month has a maximum of 31 days');
+}
