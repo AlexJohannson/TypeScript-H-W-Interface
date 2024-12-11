1ec6ae4135e07398b955620211ec5b4300b66961
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 12:18:04 +0100

    [update]:Lesson_2/Task_2-9/index.html, Main.ts, Main.js

---
+++ Lesson_2/Task_2-9/Main.ts
@@ -0,0 +1,34 @@
+interface INumberOfWeek {
+    number: number;
+}
+
+let week: INumberOfWeek = {
+    number: 3
+};
+
+
+switch (week.number) {
+    case 1:
+        console.log('Monday, consultation');
+        break;
+    case 2:
+        console.log('Tuesday, working on new lesson');
+        break;
+    case 3:
+        console.log('Wednesday, consultation');
+        break;
+    case 4:
+        console.log('Thursday, working on new lesson');
+        break;
+    case 5:
+        console.log('Friday, consultation');
+        break;
+    case 6:
+        console.log('Saturday, english language lesson');
+        break;
+    case 7:
+        console.log('Sunday, working on new lesson');
+        break;
+    default:
+        console.log('You entered an incorrect number. Enter a number from 1 to 7');
+}
