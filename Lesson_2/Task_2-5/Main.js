e85bfc56a53fba128e4e340db9939444896ae9fa
AlexJohannson <mehedsanya@gmail.com>
2024-12-11 11:34:06 +0100

    [update]:Lesson_2/Task_2-5/index.html, Main.ts, Main.js

---
+++ Lesson_2/Task_2-5/Main.js
@@ -0,0 +1,45 @@
+var temperature1 = [
+    {
+        day: 'monday',
+        morning: +7,
+        afternoon: +13,
+        evening: +8
+    },
+    {
+        day: 'tuesday',
+        morning: +4,
+        afternoon: +9,
+        evening: +3
+    },
+    {
+        day: 'wednesday',
+        morning: +2,
+        afternoon: +5,
+        evening: +2
+    },
+    {
+        day: 'thursday',
+        morning: +1,
+        afternoon: +4,
+        evening: 0
+    },
+    {
+        day: 'friday',
+        morning: -2,
+        afternoon: +3,
+        evening: 0
+    },
+    {
+        day: 'saturday',
+        morning: -2,
+        afternoon: -1,
+        evening: -1
+    },
+    {
+        day: 'sunday',
+        morning: -3,
+        afternoon: -2,
+        evening: -4
+    }
+];
+console.log(temperature1[1]);
---
+++ Lesson_2/Task_2-5/Main.ts
@@ -0,0 +1,52 @@
+interface ITemperature {
+    day: string;
+    morning: number;
+    afternoon: number;
+    evening: number;
+}
+
+let temperature1: ITemperature[] = [
+    {
+        day: 'monday',
+        morning: +7,
+        afternoon: +13,
+        evening: +8
+    },
+    {
+        day: 'tuesday',
+        morning: +4,
+        afternoon: +9,
+        evening: +3
+    },
+    {
+        day: 'wednesday',
+        morning: +2,
+        afternoon: +5,
+        evening: +2
+    },
+    {
+        day: 'thursday',
+        morning: +1,
+        afternoon: +4,
+        evening: 0
+    },
+    {
+        day: 'friday',
+        morning: -2,
+        afternoon: +3,
+        evening: 0
+    },
+    {
+        day: 'saturday',
+        morning: -2,
+        afternoon: -1,
+        evening: -1
+    },
+    {
+        day: 'sunday',
+        morning: -3,
+        afternoon: -2,
+        evening: -4
+    }
+];
+console.log(temperature1[1]);
